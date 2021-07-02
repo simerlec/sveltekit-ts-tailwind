bfa9dec4fce42be843a383ee6672fb420cc61020
Christopher Simerle <christopher@getmimo.com>
2021-07-02 13:53:12 +0200

    batch update deps, update prettier config, fix formatting, fix types

--- src/lib/Header/index.svelte
+++ src/lib/Header/index.svelte
@@ -1,93 +1,93 @@
 <script lang="ts">
-	import { page } from '$app/stores';
-	import logo from './svelte-logo.svg';
+  import { page } from '$app/stores';
+  import logo from './svelte-logo.svg';
 </script>
 
 <header class="flex justify-between">
-	<div class="w-12 h-12">
-		<a class="flex items-center justify-center w-full h-full" href="https://kit.svelte.dev">
-			<img class="w-8 h-8 object-contain" src={logo} alt="SvelteKit" />
-		</a>
-	</div>
+  <div class="w-12 h-12">
+    <a class="flex items-center justify-center w-full h-full" href="https://kit.svelte.dev">
+      <img class="w-8 h-8 object-contain" src={logo} alt="SvelteKit" />
+    </a>
+  </div>
 
-	<nav class="flex justify-center">
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z"/>
-		</svg>
-		<ul>
-			<li class:active={$page.path === '/'}><a sveltekit:prefetch href="/">Home</a></li>
-			<li class:active={$page.path === '/about'}><a sveltekit:prefetch href="/about">About</a></li>
-		</ul>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z"/>
-		</svg>
-	</nav>
+  <nav class="flex justify-center">
+    <svg viewBox="0 0 2 3" aria-hidden="true">
+      <path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
+    </svg>
+    <ul>
+      <li class:active={$page.path === '/'}><a sveltekit:prefetch href="/">Home</a></li>
+      <li class:active={$page.path === '/about'}><a sveltekit:prefetch href="/about">About</a></li>
+    </ul>
+    <svg viewBox="0 0 2 3" aria-hidden="true">
+      <path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
+    </svg>
+  </nav>
 
-	<div class="corner">
-		<!-- TODO put something else here? github link? -->
-	</div>
+  <div class="corner">
+    <!-- TODO put something else here? github link? -->
+  </div>
 </header>
 
 <style>
-	nav {
-		--background: rgba(255,255,255,0.7);
-	}
+  nav {
+    --background: rgba(255, 255, 255, 0.7);
+  }
 
-	svg {
-		width: 2em;
-		height: 3em;
-		display: block;
-	}
+  svg {
+    width: 2em;
+    height: 3em;
+    display: block;
+  }
 
-	path {
-		fill: var(--background);
-	}
+  path {
+    fill: var(--background);
+  }
 
-	ul {
-		position: relative;
-		padding: 0;
-		margin: 0;
-		height: 3em;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		list-style: none;
-		background: var(--background);
-		background-size: contain;
-	}
+  ul {
+    position: relative;
+    padding: 0;
+    margin: 0;
+    height: 3em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    list-style: none;
+    background: var(--background);
+    background-size: contain;
+  }
 
-	li {
-		position: relative;
-		height: 100%;
-	}
+  li {
+    position: relative;
+    height: 100%;
+  }
 
-	li.active::before {
-		--size: 6px;
-		content: '';
-		width: 0;
-		height: 0;
-		position: absolute;
-		top: 0;
-		left: calc(50% - var(--size));
-		border: var(--size) solid transparent;
-		border-top: var(--size) solid var(--accent-color);
-	}
+  li.active::before {
+    --size: 6px;
+    content: '';
+    width: 0;
+    height: 0;
+    position: absolute;
+    top: 0;
+    left: calc(50% - var(--size));
+    border: var(--size) solid transparent;
+    border-top: var(--size) solid var(--accent-color);
+  }
 
-	nav a {
-		display: flex;
-		height: 100%;
-		align-items: center;
-		padding: 0 1em;
-		color: var(--heading-color);
-		font-weight: 700;
-		font-size: 0.8rem;
-		text-transform: uppercase;
-		letter-spacing: 10%;
-		text-decoration: none;
-		transition: color 0.2s linear;
-	}
+  nav a {
+    display: flex;
+    height: 100%;
+    align-items: center;
+    padding: 0 1em;
+    color: var(--heading-color);
+    font-weight: 700;
+    font-size: 0.8rem;
+    text-transform: uppercase;
+    letter-spacing: 10%;
+    text-decoration: none;
+    transition: color 0.2s linear;
+  }
 
-	a:hover {
-		color: var(--accent-color);
-	}
+  a:hover {
+    color: var(--accent-color);
+  }
 </style>
--- svelte.config.js
+++ svelte.config.js
@@ -3,17 +3,17 @@ import vercel from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: sveltePreprocess(),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: sveltePreprocess(),
 
-	kit: {
-		// By default, `npm run build` will create a standard Node app.
-		// You can create optimized builds for different platforms by
-		// specifying a different adapter
-		adapter: vercel(),
+  kit: {
+    // By default, `npm run build` will create a standard Node app.
+    // You can create optimized builds for different platforms by
+    // specifying a different adapter
+    adapter: vercel(),
 
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: '#svelte'
+  }
 };
