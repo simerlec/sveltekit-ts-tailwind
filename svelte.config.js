b70d242ddac8d64b58f0b7d33d4a83e6117ea3b5
Christopher Simerle <simerle.c@gmail.com>
2022-01-09 16:55:41 +0100

    update deps

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,18 @@
-import sveltePreprocess from 'svelte-preprocess';
 import vercel from '@sveltejs/adapter-vercel';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
-export default {
+const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: sveltePreprocess(),
+  preprocess: [preprocess({})],
 
   kit: {
-    // By default, `npm run build` will create a standard Node app.
-    // You can create optimized builds for different platforms by
-    // specifying a different adapter
     adapter: vercel(),
 
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte'
   }
 };
+
+export default config;
